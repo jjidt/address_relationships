e0f7b7cf549728a4c6325f40359f26b1271352bd
J.J. Idt & Matt Waechter <student@epicodus.com>
2014-07-15 15:16:48 -0700

    input fields now automatically clear, and addresses stored for contacts

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-
   $("form#contact-info").submit(function(event) {
     event.preventDefault();
+
     var newFirstName = $("input#first-name").val();
     var newLastName = $("input#last-name").val();
     var newStreet = $("input#street").val();
@@ -13,7 +13,7 @@ $(document).ready(function(){
                        addresses: []
                      }
 
-    $('ul#contacts').append("<li>"+newPerson.firstName +
+    $('ul#contacts').append("<li class='contact'>"+newPerson.firstName +
                                  " " + newPerson.lastName +"</li>");
     $("input#first-name").val('');
     $("input#last-name").val('');
@@ -29,14 +29,14 @@ $(document).ready(function(){
         newPerson.addresses.push(newAddress);
     });
 
-    console.log(newPerson);
-
-    $('ul#contacts').click(function() {
+    $('.contact').last().click(function() {
       $('#contact-details p').text('');
       var addressInfo = newPerson.addresses.forEach(function(address){
       $('#contact-details p').append(address.street+" "+address.city+" "+ address.state +"<br />");
       });
     });
+    $("input").val("");
+
   });
 
 
@@ -46,10 +46,10 @@ $(document).ready(function(){
   //         <input id='city' type='text' placeholder='Enter a city'>
   //         <input id='state' type='text' placeholder='Enter a state'>");
   // });
-    var anotherStreet = "<input class='newStreet' type='text' placeholder='Enter a street'>"
-    var anotherCity = "<input class='newCity' type='text' placeholder='Enter a city'>"
-    var anotherState = "<input class='newState' type='text' placeholder='Enter a state'>"
-    $("div#form-container").append("<div class='new-addresses'> <form>" + anotherStreet + anotherCity + anotherState + "</form> </div>");
+  var anotherStreet = "<input class='newStreet' type='text' placeholder='Enter a street'>"
+  var anotherCity = "<input class='newCity' type='text' placeholder='Enter a city'>"
+  var anotherState = "<input class='newState' type='text' placeholder='Enter a state'>"
+  $("div#form-container").append("<div class='new-addresses'> <form>" + anotherStreet + anotherCity + anotherState + "</form> </div>");
 
   $('#another-address').click(function() {
     $("div#form-container").append("<div class='new-addresses'> <form>" + anotherStreet + anotherCity + anotherState + "</form> </div>");
